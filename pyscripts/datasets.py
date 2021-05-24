d1d08df4026b8c22e66b7390b01f4bf6f484b71e
mtnouchi <programming.nouchi.648@gmail.com>
2021-05-25 00:22:43 +0900

    prepared genome data and their deriv

--- pyscripts/datasets.py
+++ pyscripts/datasets.py
@@ -3,20 +3,51 @@ import hashlib
 import shutil
 import tempfile
 import urllib.request
+import pandas as pd
 from Bio import SeqIO
+from pathlib import Path
 from .config import path2
 
-class MetadataManager:
-    def __init__(self): pass
-    
-    def load(self, name, dirname=None):
-        metadata = None
-        if name == 'genomes':
-            metadata = pd.read_csv(
-                path2.metadata/f'NCBI_refseq_bacteria_assembly_summary_{today.strftime("%y%m%d")}.txt',
+class Metadata:
+    def __init__(self):
+        self.__asm_all   = None
+        self.__asm_inuse = None
+        self.__tax_all   = None
+        self.__tax_inuse = None
+        self.__acc       = None
+        
+    @property
+    def asm_all(self):
+        if self.__asm_all is None:
+            self.__asm_all = pd.read_csv(
+                path2.metadata/'NCBI_refseq_bacteria_assembly_summary_210520.txt',
                 sep='\t', skiprows=1, converters={'excluded_from_refseq': str}, index_col=0
             )
-        return metadata
+        return self.__asm_all
+    
+    @property
+    def asm_inuse(self):
+        if self.__asm_inuse is None:
+            self.__asm_inuse = pd.read_pickle(path2.metadata/'NCBI_asm_inuse.pkl.bz2')
+        return self.__asm_inuse
+    
+    @property
+    def tax_all(self):
+        if self.__tax_all is None:
+            self.__tax_all = pd.read_csv(path2.metadata/'GTDB_sp_clusters_r202.tsv', sep='\t')
+        return self.__tax_all
+    
+    @property
+    def tax_inuse(self):
+        if self.__tax_inuse is None:
+            self.__tax_inuse = pd.read_pickle(path2.metadata/'NCBI_asm_inuse.pkl.bz2')
+        return self.__tax_inuse
+    
+    @property
+    def acc(self):
+        if self.__acc is None:
+            self.__acc = pd.read_pickle(path2.metadata/'refseq_gtdbrep_mappings.pkl.bz2')
+        return self.__acc
 
 class DatasetDownloader:
     def __init__(self): pass
@@ -51,7 +82,7 @@ class DatasetLoader:
     def __init__(self): pass
     
     def load_genome(self, gcf, dirname=None):
-        dirname = dirname or path2.data/'pubdata'/'genomic_gbff'
+        dirname = Path(dirname or path2.data/'pubdata'/'genomic_gbff')
         
         with gzip.open(dirname/f'{gcf}.gbff.gz', 'rt') as file:
             for rec in SeqIO.parse(file, 'gb'):
